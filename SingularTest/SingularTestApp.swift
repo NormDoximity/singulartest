2d0b9775ca09e95207286b60ce346f20efdd6d06
Norm Barnard <nbarnard@doximity.com>
2024-04-04 17:18:33 -0400

    Initial Commit

---
+++ SingularTest/SingularTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingularTestApp.swift
+//  SingularTest
+//
+//  Created by Norm Barnard on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SingularTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
