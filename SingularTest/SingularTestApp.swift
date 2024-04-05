7ac318221586213c72d808bc5cb16219824f9ffd
Norm Barnard <nbarnard@doximity.com>
2024-04-05 08:40:08 -0400

    initial comit

--- SingularTest/SingularTestApp.swift
+++ SingularTest/SingularTestApp.swift
@@ -1,17 +1,15 @@
-//
-//  SingularTestApp.swift
-//  SingularTest
-//
-//  Created by Norm Barnard on 4/4/24.
-//
-
+import Singular
 import SwiftUI
 
 @main
 struct SingularTestApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().onAppear {
+                if let config = SingularConfig(apiKey: "", andSecret: "") {
+                    Singular.start(config)
+                }
+            }
         }
     }
 }
